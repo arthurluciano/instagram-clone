ef15ddb7dd6510c9700330f08a4ca5a41d6cd474
Arthur Oliveira <arthurluciano7@gmail.com>
2021-10-06 14:48:58 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import { Home } from "./ui/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/ui/Home.tsx
@@ -0,0 +1,5 @@
+
+
+export const Home: React.FC = () => {
+  return <h1>s</h1>;
+}
