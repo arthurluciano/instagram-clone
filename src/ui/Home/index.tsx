ae91dcfb2037407c1443e8864eae6f702f600e98
Arthur Oliveira <arthurluciano7@gmail.com>
2021-10-06 19:43:35 -0300

    feat: adicionando a parte de stories e posts

--- src/ui/Home/index.tsx
+++ src/ui/Home/index.tsx
@@ -1,6 +1,11 @@
 import instagramLogo from "../../assets/logo.png";
+import avatar from "../../assets/avatar.jpg";
+import postImage from "../../assets/post-image-example.jpg";
 import { GrHomeRounded } from "react-icons/gr";
 import { BiPaperPlane, BiSearch } from "react-icons/bi";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { FaRegComment } from "react-icons/fa";
+import { BsBookmark } from "react-icons/bs";
 import { AiOutlineCompass, AiOutlineHeart } from "react-icons/ai";
 import "./styles.scss";
 
@@ -30,10 +35,77 @@ export const Home: React.FC = () => {
               <li>
                 <AiOutlineHeart />
               </li>
+              <li>
+                <div className="user-configuration">
+                  <img src={avatar} alt="Foto do usuário" />
+                </div>
+              </li>
             </ul>
           </nav>
         </div>
       </header>
+      <main className="feed-container">
+        <div className="feed">
+          <section className="stories-container">
+            <div className="stories-list">
+              <div className="story">
+                <div className="story-user">
+                  <img src={avatar} alt="Foto do usuário do story" />
+                </div>
+                <span>Arthur Oliveira</span>
+              </div>
+              <div className="story">
+                <div className="story-user">
+                  <img src={avatar} alt="Foto do usuário do story" />
+                </div>
+                <span>Arthur Oliveira</span>
+              </div>
+            </div>
+          </section>
+
+          <article className="post-container">
+            <section className="post-header">
+              <div className="user-post">
+                <img src={avatar} alt="Foto do usuário" />
+                <span>Arthur Oliveira</span>
+              </div>
+
+              <FiMoreHorizontal />
+            </section>
+            <div className="post-image">
+              <img src={postImage} alt="Foto do post" />
+            </div>
+            <section className="post-buttons">
+              <div className="action-buttons">
+                <button className="button like">
+                  <AiOutlineHeart />
+                </button>
+
+                <button className="button comment">
+                  <FaRegComment />
+                </button>
+
+                <button className="button share">
+                  <BiPaperPlane />
+                </button>
+              </div>
+              <button className="save-post">
+                <BsBookmark />
+              </button>
+            </section>
+            <section className="post-feedback">
+              <img src={avatar} alt="Foto do usuário" />
+              <span className="post-users-feedback">
+                Curtido por <span>gustavoalexandre</span> e{" "}
+                <span>outras 49 pessoas</span>
+              </span>
+            </section>
+          </article>
+        </div>
+        {/* <section className="recommended">
+          
+        </section> */}
+      </main>
     </>
   );
 };
