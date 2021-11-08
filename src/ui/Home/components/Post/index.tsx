b43edc035a1c9cd6b6ed6993d7cc79f69c3c45be
arthurluciano <arthurluciano7@gmail.com>
2021-11-08 14:05:18 -0300

    feat: adicionando o desativamento do botão enquanto não houver texto a ser mandado

--- src/ui/Home/components/Post/index.tsx
+++ src/ui/Home/components/Post/index.tsx
@@ -1,30 +1,32 @@
+import { useState } from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BiPaperPlane } from "react-icons/bi";
 import { BsBookmark } from "react-icons/bs";
 import { FaRegComment } from "react-icons/fa";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { HiOutlineEmojiHappy } from "react-icons/hi";
-import avatar from "../../../../assets/avatar.jpg";
-import './styles.scss';
+import { IPost } from "../../../../shared/data/posts";
+import "./styles.scss";
 
 interface PostProps {
-  username: string;
-  postImage: string;
+  data: IPost;
 }
 
-export const Post: React.FC<PostProps> = ({ username, postImage }) => {
+export const Post: React.FC<PostProps> = ({ data }) => {
+  const [postMessage, setPostMessage] = useState('');
+
   return (
     <article className="post-container">
       <section className="post-header">
         <div className="user-post">
-          <img src={avatar} alt="Foto do usuário" />
-          <span>{username}</span>
+          <img src={data.user.avatar_url} alt="Foto do usuário" />
+          <span>{data.user.name}</span>
         </div>
 
         <FiMoreHorizontal />
       </section>
       <div className="post-image">
-        <img src={postImage} alt="Foto do post" />
+        <img src={data.photo} alt="Foto do post" />
       </div>
       <section className="post-buttons">
         <div className="action-buttons">
@@ -45,25 +47,27 @@ export const Post: React.FC<PostProps> = ({ username, postImage }) => {
         </button>
       </section>
       <section className="post-feedback">
-        <img src={avatar} alt="Foto do usuário" />
+        <img src={data.comments[0].user.avatar_url} alt="Foto do usuário" />
         <span className="post-users-feedback">
-          Curtido por <span>gustavoalexandre</span> e{" "}
-          <span>outras 49 pessoas</span>
+          Curtido por <span>{data.comments[0].user.name}</span> e{" "}
+          <span>outras {data.comments.length - 1} pessoas</span>
         </span>
       </section>
       <section className="post-comments">
         <div className="post-title">
           <span className="user-post">
-            <span className="user">{username}</span> Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod
+            <span className="user">{data.user.name}</span> {data.title}
           </span>
         </div>
-        <p className="more-comments">Ver todos os 8 comentários</p>
+        <p className="more-comments">
+          Ver todos os {data.comments.length} comentários
+        </p>
+
         <div className="comment-container">
           <div className="comment">
             <span className="user-comment">
-              <span className="user">Silva Santos</span> Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod
+              <span className="user">{data.comments[0].user.name}</span>{" "}
+              {data.comments[0].text}
             </span>
           </div>
           <button className="like-comment">
@@ -73,14 +77,15 @@ export const Post: React.FC<PostProps> = ({ username, postImage }) => {
         <div className="comment-container">
           <div className="comment">
             <span className="user-comment">
-              <span className="user">Silva Santos</span> Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod
+              <span className="user">{data.comments[1].user.name}</span>{" "}
+              {data.comments[1].text}
             </span>
           </div>
           <button className="like-comment">
             <AiOutlineHeart />
           </button>
         </div>
+
         <div className="post-date">
           <span>Há 3 horas</span>
         </div>
@@ -94,8 +99,10 @@ export const Post: React.FC<PostProps> = ({ username, postImage }) => {
             name="comment"
             className="comment"
             placeholder="Adicione um comentário..."
+            onChange={event => setPostMessage(event.target.value)}
+            value={postMessage}
           />
-          <button type="submit" className="button-submit">
+          <button type="submit" className="button-submit" disabled={!!!postMessage}>
             <span>Publicar</span>
           </button>
         </form>
